e544c403eed0678c21a4e188e60a899bdd4373be
vlad <vladlar89@gmail.com>
2025-09-29 16:53:06 +0300

    add pydantic models and basic create/update endpoints

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 # Точка входа и запуск FastAPI
 
-from db import init_db
-from handlers import create_note, get_notes, delete_note, update_note
 from fastapi import FastAPI, status
 from starlette.responses import JSONResponse
+from typing import Optional
+
+from db import init_db
+from handlers import create_note, get_notes, delete_note, update_note
+from models import CreateNote, UpdateNote
 
 init_db()
 
 app = FastAPI()
-@app.get('/')
-async def root():
-    return {'message': 'Hello, world!'}
-# print(create_note(123, 'fortnit', '19-09-2025'))
 
-# for i in get_notes(123): print(i)
+# CREATE
+@app.post('/notes/create/')
+async def create_item(note: CreateNote):
+    return create_note(note.user_id, note.note, note.deadline)
+
+# READ
 @app.get('/users/{user_id}/notes')
 async def get_items(user_id: int):
     return get_notes(user_id)
 
-# print(update_note(30, 'boblox'))
-# @app.put('')
+# UPDATE
+@app.put('/notes/update/')
+async def update_item(note: UpdateNote):
+    return update_note(note.note_id, note.note, note.deadline)
 
-# print(delete_note(10))
+# DELETE
 @app.delete('/notes/delete/{note_id}')
 async def delete_item(note_id: int):
     success = delete_note(note_id)
--- models.py
+++ models.py
@@ -1,1 +1,14 @@
 # Pydantic-модели (схемы запросов/ответов)
+
+from pydantic import BaseModel
+from typing import Optional
+
+class CreateNote(BaseModel):
+    user_id: int
+    note: str
+    deadline: str
+
+class UpdateNote(BaseModel):
+    note_id: int
+    note: Optional[str] = None
+    deadline: Optional[str] = None
